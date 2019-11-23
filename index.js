3bee68231b296d32259a22b25238020028ebf5a6
gbo96 <gbenothmen2@gmail.com>
2019-11-23 16:23:31 +0100

    fact inter

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const fact = require("./service");
+const { fact } = require("./service");
 
 const app = express();
 
--- service.js
+++ service.js
@@ -3,4 +3,15 @@ let fact = n => {
   return n * fact(n - 1);
 };
 
-module.exports = fact;
+let factIter = n => {
+  if (n === 0 || n === 1) return 1;
+  let f = 1;
+
+  for (let i = 2; i <= n; i++) {
+    f *= i;
+  }
+
+  return f;
+};
+
+module.exports = { fact, factIter };
